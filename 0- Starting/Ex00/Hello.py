bda00970613d7f899e2bc29ae30370b28af7c65d
Pei Yun Lau <plau@u91z08s01.42kl.edu.my>
2023-09-16 21:46:18 +0800

    0-Starting

---
+++ 0- Starting/Ex00/Hello.py
@@ -0,0 +1,22 @@
+
+from colour import *
+
+
+ft_list = ["Hello", "tata"]
+ft_tuple = ("Hello", "toto")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+print(GREEN + BOLD + "****** Original ******" + RESET)
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
+
+print(MAGENTA + BOLD + "****** Exercise ******" + RESET)
+ft_list[1] = "World"
+ft_tuple = ("Hello", "Malaysia")
+ft_set = {"Hello", "Kuala Lumpur"}
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
