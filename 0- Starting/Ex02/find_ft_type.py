68496c4748d0c3f9f7c7af62d30951158d5df058
Pei Yun Lau <plau@u91z08s01.42kl.edu.my>
2023-09-18 15:09:06 +0800

    ex02 done

---
+++ 0- Starting/Ex02/find_ft_type.py
@@ -0,0 +1,12 @@
+
+# this function accepts 1 argument named object of any type
+# return an int 
+def all_thing_is_obj(object: any) -> int:
+	type_of_parameter = type(object)
+	if (isinstance(object, str)):
+		print(object, "is in the kitchen :", type_of_parameter)
+	elif (isinstance(object, int)):
+		print("Type not found")
+	else:
+		print(type_of_parameter.__name__.capitalize(), ":", type_of_parameter)
+	return 42
