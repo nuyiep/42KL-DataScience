5942962313176ed1f5c2e76649df325f8ea2926e
Pei Yun Lau <plau@u91z08s01.42kl.edu.my>
2023-09-18 17:11:01 +0800

    fix norm

--- 0- Starting/Ex00/Hello.py
+++ 0- Starting/Ex00/Hello.py
@@ -5,7 +5,7 @@ from colour import *
 ft_list = ["Hello", "tata"]
 ft_tuple = ("Hello", "toto")
 ft_set = {"Hello", "tutu!"}
-ft_dict = {"Hello" : "titi!"}
+ft_dict = {"Hello": "titi!"}
 
 # print(GREEN + BOLD + "****** Original ******" + RESET)
 # print(ft_list)
--- 0- Starting/ex01/format_ft_time.py
+++ 0- Starting/ex01/format_ft_time.py
@@ -7,8 +7,8 @@ formatted_timestamp = f"{timestamp:,.4f}"
 # print(formatted_timestamp)
 formatted_scientific = f"{timestamp:.2e}"
 # print(formatted_scientific)
-print("Seconds since January 1, 1970: ", formatted_timestamp, "or", formatted_scientific, "in scientific notation")
-
+print("Seconds since January 1, 1970: ", formatted_timestamp, "or",
+formatted_scientific, "in scientific notation")
 
 # Oct 21 2022
 # b - months abbreviation
@@ -26,4 +26,5 @@ print(formatted_date)
 # print(formatted_time)
 
 # epoch - begins at 00:00:00 1 Jan 1970
-# Seconds since January 1, 1970: 1,666,355,857.3622 or 1.67e+09 in scientific notation$
+# Seconds since January 1, 1970: 1,666,355,857.3622 or
+# 1.67e+09 in scientific notation$
