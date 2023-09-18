8c4611d4cc63a625e0a842327065f326c4484c18
Pei Yun Lau <plau@u91z08s01.42kl.edu.my>
2023-09-18 15:39:51 +0800

    ex03 done

---
+++ 0- Starting/Ex03/NULL_not_found.py
@@ -0,0 +1,19 @@
+
+def NULL_not_found(object: any) -> int:
+	if (object is None):
+		print("Nothing:", object, type(object))
+	elif (isinstance(object, float)):
+		print("Cheese:", object, type(object))
+		return 0
+	elif (isinstance(object, bool)):
+		print("Fake:", object ,type(object))
+		return 0
+	elif (isinstance(object, int) and object == 0):
+		print("Zero:", object, type(object))
+		return 0
+	elif (isinstance(object, str) and object == ''):
+		print("Empty:", type(object))
+		return 0
+	else:
+		print("Type not found")
+		return 1
