3818e36f3649df65e03ae8bfa227f1e42d57d51d
Pei Yun Lau <plau@u91z08s01.42kl.edu.my>
2023-09-19 15:43:24 +0800

    ex06 donee

--- 0- Starting/ex06/filterstring.py
+++ 0- Starting/ex06/filterstring.py
@@ -1,25 +1,47 @@
 
 import sys
 
-def main():
+def format_checking():
+	'''
+		Check if the first argv is a string
+		Check if the second argv is an int
+		If not, exit the program
+	'''
 	if (len(sys.argv) != 3):
 		print("AssertionError: the arguments are bad")
 		sys.exit()
 	if (sys.argv[1] == None or sys.argv[2] == None):
 		print("AssertionError: empty content")
 		sys.exit()
+	for char in sys.argv[1]:
+		if (char.isalpha() == False and char != ' '):
+			print("AssertionError: not an alphabet")
+			sys.exit()
 	if (sys.argv[2].isdigit() == False):
 		print("AssertionError: the arguments are bad- not digit")
 		sys.exit()
+
+def ft_filter():
+	'''
+		in python- filter(function, iterable)
+		returns items from iterable based on some criteria
+	'''
 	word_list = sys.argv[1].split(' ')
-	all_word_count = []
-	for word in word_list:
-		if word.isalpha() == False:
-			print("AssertionError: the arguments are bad- not alpha")
-			sys.exit()
-		word_count = len(word)
-		if (word_count > int(sys.argv[2])):
-			all_word_count.append(word)
+	all_word_count = [word for word in word_list 
+				   if (lambda word: len(word) > int(sys.argv[2]))(word)]
+	# for word in word_list:
+	# 	word_count = len(word)
+	# 	if (word_count > int(sys.argv[2])):
+	# 		all_word_count.append(word)
+	return all_word_count
+
+def main():
+	'''
+		format checking then filter the words
+		then print
+	'''
+	format_checking()
+	all_word_count = ft_filter()
 	print(all_word_count)
 
 if __name__ == "__main__":
