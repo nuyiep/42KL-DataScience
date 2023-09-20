900804038f847caa82b52d7e55783b8f586fe5aa
Pei Yun Lau <plau@u91z08s01.42kl.edu.my>
2023-09-20 21:19:44 +0800

    ex09

--- 0- Starting/ex09/setup.py
+++ 0- Starting/ex09/setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="ft_package",
     version="0.0.1",
-    summary="A sample test package",
-    home_page="https://github.com/nuyiep/42KL-DataScience",
+    description="A sample test package",
+    url="https://github.com/nuyiep/42KL-DataScience",
     author="the-cool-girl",
     author_email="plau@42.fr",
     license="MIT",
